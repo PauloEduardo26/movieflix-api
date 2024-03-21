6aac30fdbeac13af973da6efe8e64dd7294f61d4
Paulo Eduardo <pauloeduardosozza73@gmailcom>
2024-03-21 11:15:49 -0300

    adicionada remocao de filmes

--- src/server.ts
+++ src/server.ts
@@ -86,6 +86,25 @@ app.put("/movies/:id", async (req, res) => {
     res.status(200).send("atualizacao de certo");
 });
 
+app.delete("/movies/:id", async (req, res) => {
+    const id = Number(req.params.id);
+
+    try{
+        const movie = await prisma.movie.findUnique({ where: { id } });
+
+        if(!movie){ 
+            return res.status(404).send({ message: "filme nao encontrado" });
+        }
+
+        await prisma.movie.delete({ where: { id } });
+        
+    }catch(error){
+        return res.status(500).send({ message: "nao foi possivel remover o filme" });
+    }
+
+    res.status(200).send();
+});
+
 app.listen(port, () => {
     console.log(`Servidor em execução em http://localhost:${port}`);
 });
